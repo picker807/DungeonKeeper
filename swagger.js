bfc421789405813c94504e7b77cd1a8372cbebfa
Blahdidah <80852722+Blahdidah@users.noreply.github.com>
2023-11-15 18:05:01 -0700

    added swagger auto-gen to the project

---
+++ swagger.js
@@ -0,0 +1,15 @@
+const swaggerAutogen = require('swagger-autogen')();
+
+const doc={
+    info:{
+        title:'Dungeon Keeper',
+        description:'A database that organizes spells for Advanced Dungeons and Dragons Second Edition. Users can view spells, create characters and organize spellbooks based on their character.',
+    },
+    host:'localhost:8000',//we need a link to the render for whoever's account is setting this up
+    schemes:['http','https']
+};
+
+const outputFile= './swagger.json';
+const endpointFile = ['./routes/index.js'];
+
+swaggerAutogen(outputFile, endpointFile, doc)
