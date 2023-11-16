ecac395096cd1fc79619e41f598097674214c506
zakerysattler@yahoo.com <zakerysattler@yahoo.com>
2023-11-16 11:37:00 -0500

    swagger changes

--- server.js
+++ server.js
@@ -1,89 +1,87 @@
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
 const bodyParser = require("body-parser");
 const port = process.env.PORT || 3000;
-const cors = require('cors');
-const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('../swagger.json');
-const db = require('./models');
+const cors = require("cors");
+const db = require("./models");
 
-const passport = require('./config/passport');
-const session = require('express-session');
-const crypto = require('crypto');
+const passport = require("./config/passport");
+const session = require("express-session");
+const crypto = require("crypto");
 
 const app = express();
-const secret = crypto.randomBytes(64).toString('hex');
+const secret = crypto.randomBytes(64).toString("hex");
 
 app
-    .use(session({
-        secret: secret,
-        resave: true,
-        saveUninitialized: true
-    }))
-
-    // Passport Initialization Middleware
-    //.use(passport.initialize())
-    //.use(passport.session())
-// Routes
-    .use('/', require('./routes'))
-    .use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
-    .use(cors())
-    .use(bodyParser.json())
-
-    .use((req, res, next) => {
-        res.setHeader('Access-Control-Allow-Origin', '*');
-        res.setHeader(
-            'Access-Control-Allow-Headers',
-            'Origin, X-Requested-With, Content-Type, Accept, Z-Key'
-        );
-        res.setHeader('Content-Type', 'application/json');
-        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-        next();
+  .use(
+    session({
+      secret: secret,
+      resave: true,
+      saveUninitialized: true,
     })
+  )
 
-    //.use(express.json())
-    
+  // Passport Initialization Middleware
+  //.use(passport.initialize())
+  //.use(passport.session())
+  // Routes
+  .use("/", require("./routes"))
+  .use(cors())
+  .use(bodyParser.json())
 
-    
+  .use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept, Z-Key"
+    );
+    res.setHeader("Content-Type", "application/json");
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET, POST, PUT, DELETE, OPTIONS"
+    );
+    next();
+  });
 
-    // view engine setup
-    //app.set('views', path.join(__dirname, 'views'));
-    //app.set('view engine', 'pug');
+//.use(express.json())
 
-    
+// view engine setup
+//app.set('views', path.join(__dirname, 'views'));
+//app.set('view engine', 'pug');
+
 //.use(express.static(path.join(__dirname, 'public'))
 
 // catch 404 and forward to error handler
 //app.use(function(req, res, next) {
-  //  next(createError(404));
+//  next(createError(404));
 //});
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
-    res.status(err.status || 500);
-    res.render('error');
+  res.status(err.status || 500);
+  res.render("error");
 });
 
 // Connect to Database
 db.mongoose
-    .connect(db.url, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    })
-    .then(() => {
-        app.listen(port, () => {
-            console.log(`DB connected and Server is running on port ${port}`);
-        });
-    })
-    .catch((err) => {
-        console.log('Cannot connect to database.', err);
-        process.exit();
-    })
+  .connect(db.url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`DB connected and Server is running on port ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.log("Cannot connect to database.", err);
+    process.exit();
+  });
 
 module.exports = app;
