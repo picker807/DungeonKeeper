614fbad2a7048b1a9d70a493f90e9d5331175a94
Blahdidah <80852722+Blahdidah@users.noreply.github.com>
2023-11-15 19:50:23 -0700

    Updated Swagger to allow get to work

--- swagger.js
+++ swagger.js
@@ -5,11 +5,11 @@ const doc={
         title:'Dungeon Keeper',
         description:'A database that organizes spells for Advanced Dungeons and Dragons Second Edition. Users can view spells, create characters and organize spellbooks based on their character.',
     },
-    host:'localhost:8000',//we need a link to the render for whoever's account is setting this up
+    host:'localhost:3000',//we need a link to the render for whoever's account is setting this up
     schemes:['http','https']
 };
 
-const outputFile= './swagger.json';
-const endpointFile = ['./routes/index.js'];
+const outputFile= './swagger1.json';
+const endpointFile = ['./routes/spells.js' ];
 
 swaggerAutogen(outputFile, endpointFile, doc)
