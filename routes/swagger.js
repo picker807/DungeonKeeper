fb599087c09b0e29aa712ac7953ad136d688eb8d
zakerysattler@yahoo.com <zakerysattler@yahoo.com>
2023-11-17 10:20:02 -0500

    switch serve

--- routes/swagger.js
+++ routes/swagger.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const swaggerUi = require("swagger-ui-express");
 const swaggerDoc = require("../swagger.json");
 
-router.use("/api-docs", swaggerUi.serve);
 router.get("/api-docs", swaggerUi.setup(swaggerDoc));
+router.use("/api-docs", swaggerUi.serve);
 
 module.exports = router;
