3a8b69668ff049cf5e942d18caab899a880d072c
zakerysattler@yahoo.com <zakerysattler@yahoo.com>
2023-11-17 10:23:48 -0500

    Changed host

--- swagger.js
+++ swagger.js
@@ -1,15 +1,16 @@
-const swaggerAutogen = require('swagger-autogen')();
+const swaggerAutogen = require("swagger-autogen");
 
-const doc={
-    info:{
-        title:'Dungeon Keeper',
-        description:'A database that organizes spells for Advanced Dungeons and Dragons Second Edition. Users can view spells, create characters and organize spellbooks based on their character.',
-    },
-    host:'localhost:3000',//we need a link to the render for whoever's account is setting this up
-    schemes:['http','https']
+const doc = {
+  info: {
+    title: "Dungeon Keeper",
+    description:
+      "A database that organizes spells for Advanced Dungeons and Dragons Second Edition. Users can view spells, create characters and organize spellbooks based on their character.",
+  },
+  host: "https://dungeon-keeper.onrender.com", //we need a link to the render for whoever's account is setting this up
+  schemes: ["https"],
 };
 
-const outputFile= './swagger.json';
-const endpointFile = ['./routes/index.js' ];
+const outputFile = "./swagger.json";
+const endpointFile = ["./routes/index.js"];
 
-swaggerAutogen(outputFile, endpointFile, doc)
+swaggerAutogen(outputFile, endpointFile, doc);
