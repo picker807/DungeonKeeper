645c32d15e01479fcd6e189b365d8b588410fa48
zakerysattler@yahoo.com <zakerysattler@yahoo.com>
2023-11-22 10:52:30 -0500

    error handler

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const bodyParser = require("body-parser");
 const port = process.env.PORT || 3000;
 const cors = require("cors");
 const db = require("./models");
-const errorHandler = require("./middleware/middleware");
+const errorHandler = require("./middleware/errorHandler");
 
 const passport = require("./config/passport");
 const session = require("express-session");
