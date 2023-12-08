d29aa2b13813c98070bd4055f4daeecfa62ea11e
zakerysattler@yahoo.com <zakerysattler@yahoo.com>
2023-12-08 14:17:52 -0500

    moved function back

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ const { auth, requiresAuth } = require("express-openid-connect");
 const db = require("./models");
 const errorHandler = require("./middleware/errorHandler");
 const port = process.env.PORT || 3000;
-const { createAutomaticUser } = require("./middleware/createAutomaticUser");
 
 const app = express();
 const secret = crypto.randomBytes(64).toString("hex");
@@ -40,6 +39,33 @@ app.use(express.json());
 if (process.env.NODE_ENV !== "test") {
   app.use(auth(config));
 }
+
+// Function to create a user immediately after authentication
+const createAutomaticUser = async (req, res, next) => {
+  const User = db.user;
+
+  try {
+    const { sub, email } = req.oidc.user;
+
+    // Check if the user already exists in the database
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      return next();
+    }
+
+    // Create a new user
+    const newUser = new User({
+      username: sub,
+      email,
+    });
+
+    await newUser.save();
+    next();
+  } catch (err) {
+    next(err);
+  }
+};
 
 // Apply the createAutomaticUser middleware after authentication
 app.use(requiresAuth(), createAutomaticUser);
